50621f793c5131ffe03e27660e1b16f033a7b151
daniloperoj-commits <daniloperoj@gmail.com>
2026-05-05 23:07:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -668,7 +668,7 @@ if st.button("Buscar"):
 st.markdown("------")
 
 st.markdown(
-    "<p style='text-align:center; font-size:12px; color:gray;'>v2.0-BETA</p>",
+    "<p style='text-align:center; font-size:12px; color:gray;'>v2.0-BETA ()</p>",
     unsafe_allow_html=True
 )
 
